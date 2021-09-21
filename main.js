87e5e57f9896acea0ad5a227b7d6f9cf16307c69
David Thomas <david.thomas1986@gmail.com>
2021-09-21 17:29:33 +0100

    added javascript functionality

--- main.js
+++ main.js
@@ -3,5 +3,49 @@ const operatorButtons = document.querySelectorAll('.data-operation');
 const equalButton = document.querySelector('.data-equals');
 const deleteButton = document.querySelector('.data-delete');
 const clearButton = document.querySelector('.data-clear');
+const previousOperandTextElement = document.querySelector('.data-previous-operand');
+const currentOperandTextElement = document.querySelector('.data-current-operand');
 
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+};
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+});
+
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+};
+
+delete() {
+
+};
+
+appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+};
+
+chooseOperation(operation) {
+
+};
+
+compute() {
+
+};
+
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+};

4a58df2bcee43a825e1c07c1fe38abe8ce4ec16b
David Thomas <david.thomas1986@gmail.com>
2021-09-22 22:08:39 +0100

    fixed query selectors. never added full stop before class name

--- main.js
+++ main.js
@@ -86,13 +86,13 @@ class Calculator {
   }
   
   
-  const numberButtons = document.querySelectorAll('data-number')
-  const operationButtons = document.querySelectorAll('data-operation')
-  const equalsButton = document.querySelector('data-equals')
-  const deleteButton = document.querySelector('data-delete')
-  const allClearButton = document.querySelector('data-clear')
-  const previousOperandTextElement = document.querySelector('data-previous-operand')
-  const currentOperandTextElement = document.querySelector('data-current-operand')
+  const numberButtons = document.querySelectorAll('.data-number')
+  const operationButtons = document.querySelectorAll('.data-operation')
+  const equalsButton = document.querySelector('.data-equals')
+  const deleteButton = document.querySelector('.data-delete')
+  const allClearButton = document.querySelector('.data-clear')
+  const previousOperandTextElement = document.querySelector('.data-previous-operand')
+  const currentOperandTextElement = document.querySelector('.data-current-operand')
   
   const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
   
